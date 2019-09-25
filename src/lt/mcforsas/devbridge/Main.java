bcdc86996d314a525ce60dd156b966f92323c438
MCForsas <karolis.poviliunas@gmail.com>
2019-09-25 16:45:18 +0300

    Added description and methods
    Added desciption with info, what the program is about.
    Added method isMagicNumber.
    Added method maxMultiplicator.
    Added method isMagicNumberMultiple.
    Added input scanning in main

--- src/lt/mcforsas/devbridge/Main.java
+++ src/lt/mcforsas/devbridge/Main.java
@@ -1,7 +1,78 @@
+/**
+ * Checks if number is magic.
+ * A magic number is an integer in which permutations of the digits are successive multiples of the number (number of
+ * digits and order does not change, but can start from different position).
+ *
+ * The most widely known is 142857:
+ * 142857 × 1 = 142857
+ * 142857 × 2 = 285714
+ * 142857 × 3 = 428571
+ * 142857 × 4 = 571428
+ * 142857 × 5 = 714285
+ * 142857 × 6 = 857142
+ *
+ * Input Data:
+ * > Input a number: 142857
+ * Expected Output
+ * > It's magic!
+ *
+ */
 package lt.mcforsas.devbridge;
+
+import java.util.Scanner;
 
 public class Main {
 
+    //TODO: add documentation to all methods.
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.nextLine();
+
+            if(isMagicNumber(number)){
+            System.out.println("It's magic!");
+        }else {
+            System.out.println("How unfortunate. It's not magic :C");
+        }
+    }
+
+
+    private static boolean isMagicNumber(String number) {
+        //Parse number into int
+        int numberInt;
+        try {
+            numberInt = Integer.parseInt(number);
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+            return false;
+        }
+
+        int maxMultiplicator = maxMultiplicator(numberInt);
+
+        for(int i = 2; i < maxMultiplicator; i++){
+
+        }
+
+        return true;
+    }
+
+    private static int maxMultiplicator(Integer number){
+        int maxMultiplicator = 1;
+        //Loop from 2 to 9, because multiplying by a smaller number will always produce smaller or equal number, and
+        // greater than 9 will always prduce bigger number
+        for(int i = 2; i > 9; i++){
+            String multiplied = Integer.toString(number * i);
+            if(multiplied.length() <= number.toString().length()){
+                maxMultiplicator = i;
+            }else{
+                break;
+            }
+        }
+
+        return maxMultiplicator;
+    }
+
+    public static boolean isMagicNumbersMultiple(String magicNumber, String multiple){
+
+        return false;
     }
 }

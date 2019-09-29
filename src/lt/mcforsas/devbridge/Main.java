ebf9a8ab8ff6f8305551cdd41626fc1568247d85
MCForsas <karolis.poviliunas@gmail.com>
2019-09-29 07:12:29 +0300

    Fixed isMagicNumbersMultiple method and intorduced local variables for debugging.
    isMagicNumbersMultiple now works as expected
    added isMagic to isMagicNumber() for debugging
    Added asking for another number

--- src/lt/mcforsas/devbridge/Main.java
+++ src/lt/mcforsas/devbridge/Main.java
@@ -28,10 +28,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String number = scanner.nextLine();
 
-            if(isMagicNumber(number)){
+        if(isMagicNumber(number)){
             System.out.println("It's magic!");
         }else {
-            System.out.println("How unfortunate. It's not magic :C");
+            System.out.println("How unfortunate. It's not magic :C. Try another one? Y/N");
+        }
+        //TODO: remove asking for another number
+        if(scanner.nextLine().equals("y")){
+            main(args);
         }
     }
 
@@ -48,8 +52,10 @@ public class Main {
 
         int maxMultiplicator = maxMultiplicator(numberInt);
 
+
         for(int i = 2; i < maxMultiplicator; i++){
-            if(!isMagicNumbersMultiple(number,String.valueOf(numberInt*i))){
+            boolean isMagic = isMagicNumbersMultiple(number,String.valueOf(numberInt*i));
+            if(!isMagic){
                 return false;
             }
         }
@@ -76,8 +82,11 @@ public class Main {
     public static boolean isMagicNumbersMultiple(String magicNumber, String multiple){
         String searchString;
         int numberLength = magicNumber.length();
-        for(int i = 0; i < numberLength; i++){
-            searchString = (magicNumber.substring(i + (numberLength - i)) + magicNumber.substring(0,i));
+        for(int i = 0; i < numberLength-1; i++){
+            String firstHalf = multiple.substring(i+1);
+            String secodHalf = multiple.substring(0,i+1);
+            searchString = firstHalf.concat(secodHalf);
+
             if(searchString.equals(magicNumber)){
                 return true;
             }
